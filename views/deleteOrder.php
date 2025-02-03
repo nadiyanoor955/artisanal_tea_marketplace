fe3aca5034db6b46381d2bd330ddbd5f4490b76d
nadiyanoor955 <nadiyanoor2020@gmail.com>
2025-02-03 09:59:56 +0600

    Update deleteOrder.php

--- views/deleteOrder.php
+++ views/deleteOrder.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="../JS/myJS.js"></script>
+    
 </head>
 
 </head>
@@ -12,7 +12,7 @@
     <form action="../controllers/deleteOrder_Control.php" method="POST">
         <h2>Delete Order</h2>
         <label for="OrderID">Enter Order ID to Delete:</label>
-        <input type="number" id="OrderID" name="OrderID" placeholder="Enter Order ID" required>
+        <input type="number" id="OrderID" name="OrderID" placeholder="Enter Order ID">
         <button type="submit">Delete Order</button>
     </form>
 </body>
