0a6ea0751b2b32df014d3654667fb906ce42d7cd
nadiyanoor955 <nadiyanoor2020@gmail.com>
2025-02-03 10:00:52 +0600

    Update updateOrderStatus.php

--- views/updateOrderStatus.php
+++ views/updateOrderStatus.php
@@ -13,10 +13,10 @@
     
         <h2>Update Order Status</h2>
         <label for="OrderID">Order ID:</label>
-        <input type="number" id="OrderID" name="OrderID" placeholder="Enter Order ID" required>
+        <input type="number" id="OrderID" name="OrderID" placeholder="Enter Order ID">
 </br>
         <label for="OrderStatus">New Order Status:</label>
-        <select id="OrderStatus" name="OrderStatus" required>
+        <select id="OrderStatus" name="OrderStatus">
             <option value="" disabled selected>Select Status</option>
             <option value="Pending">Pending</option>
             <option value="Processing">Processing</option>
@@ -25,7 +25,7 @@
         </select>
 
         <label for="PaymentStatus">New Payment Status:</label>
-        <select id="PaymentStatus" name="PaymentStatus" required>
+        <select id="PaymentStatus" name="PaymentStatus">
             <option value="" disabled selected>Select Status</option>
             <option value="Pending">Pending</option>
             <option value="Paid">Paid</option>
