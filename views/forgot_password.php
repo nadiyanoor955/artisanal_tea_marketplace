27bdc16fef9a8d16bbde9845010007acb9bc946f
nadiyanoor955 <nadiyanoor2020@gmail.com>
2025-02-03 09:59:14 +0600

    Update forgot_password.php

--- views/forgot_password.php
+++ views/forgot_password.php
@@ -13,8 +13,11 @@
     <h2>Forgot Password</h2>
     <p>Enter your registered email address, and we'll send you a link to reset your password.</p>
     <form action="process_forgot_password.php" method="POST">
-        <input type="email" name="email" placeholder="Enter your email" required>
-        <button type="submit">Send Reset Link</button>
+        <input type="email" name="email" placeholder="Enter your email">
+
+<button onclick="location.href='forgotPin.php'">
+    Send Reset Link
+</button>
     </form>
     <a href="login.php">Back to Login</a>
 </div>
